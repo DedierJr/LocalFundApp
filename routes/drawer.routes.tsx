cc0638dfc195cc4c899ca2145f13ea31a3d7801a
DedierJr <dedieraguiar@gmail.com>
2024-04-08 17:32:37 -0300

    despair commit 2

--- routes/drawer.routes.tsx
+++ routes/drawer.routes.tsx
@@ -10,21 +10,21 @@ import ManterMarcador from '../screens/ManterMarcador';
 import Mapa from '../screens/Mapa';
 import TabRoutes from './tab.routes';
 
-function ManterMarcadorScreen({ navigation }) {
+function ManterMarcadorScreen() {
   return (
-   <ManterMarcador></ManterMarcador>
+   <ManterMarcador />
   );
 }
 
-function MapaScreen({ navigation }) {
+function MapaScreen() {
   return (
-   <Mapa></Mapa>
+   <Mapa />
   );
 }
 
 const Drawer = createDrawerNavigator();
 
-export default function TabDrawer() {
+export default function DrawerRoutes() {
   return (
       <Drawer.Navigator initialRouteName="Registro">
         <Drawer.Screen
@@ -34,9 +34,10 @@ export default function TabDrawer() {
         />
         <Drawer.Screen name="Registro" component={RegistroScreen} />
         <Drawer.Screen name="Login" component={Login} />
-        <Drawer.Screen name="Adicionar Post" component={TabRoutes} initialParams={{tab: "Add Post"}}
+        <Drawer.Screen name="Perfil" component={TabRoutes} initialParams={{tab: "Perfil"}}
 />
         <Drawer.Screen name="Listar Posts" component={ListarPosts} />
+        <Drawer.Screen name="Adicionar Post" component={AdicionarPost} />
         <Drawer.Screen name="Manter Marcador" component={ManterMarcadorScreen} />
         <Drawer.Screen name="Mapa" component={MapaScreen} />
       </Drawer.Navigator>
