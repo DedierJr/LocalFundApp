7642a91780c4c5914f12b7b51aea3198a877b975
DedierJr <dedieraguiar@gmail.com>
2024-04-08 17:38:26 -0300

    stack aparentemente implementado

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
 // /home/aluno/Documentos/DedierJr/LocalFundApp/App.tsx
 import 'react-native-gesture-handler'
-
 import Routes from './routes'
 
 export default function App() {
-  return(
-    <Routes />
-  )
+    return(
+        <Routes />
+    )
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,13 +1,17 @@
 // /home/aluno/Documentos/DedierJr/LocalFundApp/routes/index.tsx
 import { NavigationContainer } from "@react-navigation/native";
-
+import { createStackNavigator } from "@react-navigation/stack";
 import DrawerRoutes from "./drawer.routes";
 
+const Stack = createStackNavigator();
 
-export default function Routes(){
+export default function Routes() {
     return (
         <NavigationContainer>
-            <DrawerRoutes />
+            <Stack.Navigator initialRouteName="DrawerRoutes" screenOptions={{headerShown: false}}>
+                <Stack.Screen name="DrawerRoutes" component={DrawerRoutes} />
+                {/* Adicione outras telas Stack.Screen conforme necessário */}
+            </Stack.Navigator>
         </NavigationContainer>
-    )
+    );
 }
